f778d95d4ff82fc6eefd4c4b91279c1ec5eb6b46
m1stadev <adamhamdi31@gmail.com>
2026-06-26 17:41:49 -0500

    insns: add WDR instruction

--- binavry/insns/ctrl.py
+++ binavry/insns/ctrl.py
@@ -4,4 +4,5 @@ _CTRL_INSNS = (
     InstructionData(name='BREAK', mnem='break', sig='1001010110011000'),
     InstructionData(name='NOP',   mnem='nop',   sig='0000000000000000'),
     InstructionData(name='SLEEP', mnem='sleep', sig='1001010110001000'),
+    InstructionData(name='WDR',   mnem='wdr',   sig='1001010110101000'),
     )
