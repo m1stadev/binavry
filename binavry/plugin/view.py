ebc9bd56dbeeab0ea732160e24357ebc00b61d32
m1stadev <adamhamdi31@gmail.com>
2026-06-26 16:01:17 -0500

    pre-commit: update hooks

--- binavry/plugin/view.py
+++ binavry/plugin/view.py
@@ -36,7 +36,7 @@ class AVRView(BinaryView):
 
     def init(self) -> bool:
         self.arch = Architecture['AVR']
-        self.platform = self.arch.standalone_platform  # ty:ignore[unresolved-attribute]
+        self.platform = self.arch.standalone_platform
         # ROM
         self.add_auto_segment(
             start=0,
