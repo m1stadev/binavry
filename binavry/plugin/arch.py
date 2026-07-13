973c5480b5291c2bef63c4286da88555bc95f7f0
m1stadev <adamhamdi31@gmail.com>
2026-07-13 18:28:04 -0500

    arch: implement analyze_basic_blocks() & get_instruction_text_with_context() I'm using a custom implementation of analyze_custom_blocks() to store IO regs from the BinaryView in the function context for IN/OUT instructions, which gets applied in get_instruction_text_with_context()

--- binavry/plugin/arch.py
+++ binavry/plugin/arch.py
@@ -1,14 +1,22 @@
 from binaryninja import (
+    ArchAndAddr,
     Architecture,
+    BasicBlock,
+    BasicBlockAnalysisContext,
     BranchType,
+    Function,
     InstructionInfo,
     InstructionTextToken,
     InstructionTextTokenType,
     LowLevelILFunction,
     RegisterInfo,
+    RegisterName,
+    Symbol,
+    SymbolType,
 )
+from binaryninja.lowlevelil import ExpressionIndex
 
-from . import Instruction, Instructions, OpType, get_base_insn
+from . import Instruction, Instructions, Operand, OpType, get_base_insn
 
 
 class AVRArch(Architecture):
@@ -60,6 +68,132 @@ class AVRArch(Architecture):
     global_regs = ['r0', 'r1']
     stack_pointer = 'SP'
 
+    def analyze_basic_blocks(
+        self, func: Function, context: BasicBlockAnalysisContext
+    ) -> None:
+        # NOTE: The entry point does not call this function,
+        # so IO regs are not displayed correctly in the RESET irq
+
+        data = func.view
+        blocks_to_process = [func.start]
+        finished = []
+        arch_context = {'mapped_io': {}}
+
+        while len(blocks_to_process) > 0:
+            if data.analysis_is_aborted:
+                break
+
+            addr = blocks_to_process.pop()
+            if addr in finished:
+                continue
+
+            finished.append(addr)
+
+            block: BasicBlock = context.create_basic_block(func.arch, addr)  # ty:ignore[invalid-assignment]
+            while True:
+                try:
+                    insn = Instruction.decode(data.read(addr, 4))
+                except ValueError:
+                    break
+
+                block.add_instruction_data(insn.data)
+
+                idata = Instructions(get_base_insn(insn.idata) or insn.idata)
+                if idata not in (
+                    Instructions.BRBC,
+                    Instructions.BRBS,
+                    Instructions.JMP,
+                    Instructions.RET,
+                    Instructions.RETI,
+                    Instructions.RJMP,
+                ):
+                    match idata:
+                        case Instructions.IN | Instructions.OUT:
+                            io_offset = data.get_section_by_name('ROM').length
+                            op = next(
+                                op
+                                for op in insn.operands
+                                if op.op_type == OpType.ADDR_IO
+                            )
+                            io_reg = data.get_symbol_at(
+                                io_offset + op.value + 0x20,
+                                namespace=SymbolType.DataSymbol,
+                            )
+                            if io_reg is None:
+                                # Wide register?
+                                io_reg = data.get_symbol_at(
+                                    io_offset + op.value + 0x19,
+                                    namespace=SymbolType.DataSymbol,
+                                )
+
+                            arch_context['mapped_io'][addr] = {
+                                'name': io_reg.short_name,
+                                'addr': io_reg.address,
+                            }
+
+                        case Instructions.CALL | Instructions.RCALL:
+                            if idata == Instructions.RCALL:
+                                val = addr + insn.operands[-1].value
+                            else:
+                                val = insn.operands[-1].value
+
+                            data.define_auto_symbol_and_var_or_function(
+                                Symbol(SymbolType.FunctionSymbol, val, f'sub_{val:x}')
+                            )
+                            data.add_function(val)
+                            context.add_direct_code_reference(
+                                val, ArchAndAddr(self, addr)
+                            )
+
+                    addr += len(insn.data)
+                    continue
+
+                match idata:
+                    case Instructions.JMP | Instructions.RJMP:
+                        val = insn.operands[0].value
+                        if insn.idata == Instructions.RJMP:
+                            val += addr
+
+                        blocks_to_process.append(val)
+                        block.add_pending_outgoing_edge(
+                            BranchType.UnconditionalBranch,
+                            val,
+                            func.arch,
+                        )
+
+                    case Instructions.BRBC | Instructions.BRBS:
+                        val = addr + insn.operands[-1].value
+                        blocks_to_process += [val, addr + 2]
+
+                        block.add_pending_outgoing_edge(
+                            BranchType.TrueBranch,
+                            val,
+                            func.arch,
+                        )
+
+                        block.add_pending_outgoing_edge(
+                            BranchType.FalseBranch,
+                            addr + 2,
+                            func.arch,
+                        )
+
+                    # case (
+                    #    Instructions.EICALL
+                    #    | Instructions.EIJMP
+                    #    | Instructions.ICALL
+                    #    | Instructions.IJMP
+                    # ):
+                    #    info.add_branch(BranchType.IndirectBranch)
+
+                block.end = addr + len(insn.data)
+                context.add_basic_block(block)
+                break
+
+        if len(arch_context['mapped_io']) > 0:
+            context.function_arch_context = arch_context
+
+        context.finalize()
+
     def get_instruction_info(self, data: bytes, addr: int) -> InstructionInfo | None:
         try:
             insn = Instruction.decode(data)
@@ -102,13 +236,37 @@ class AVRArch(Architecture):
 
         return info
 
-    def get_instruction_text(
-        self, data: bytes, addr: int
+    def get_instruction_text_with_context(
+        self, data: bytes, addr: int, context: dict | None
     ) -> tuple[list[InstructionTextToken], int] | None:
         try:
             insn = Instruction.decode(data)
         except ValueError:
             return None
+
+        tokens, length = self.get_instruction_text(data, addr, insn=insn)  # ty:ignore[not-iterable]
+
+        if (context is not None) and (addr in context['mapped_io'].keys()):
+            io_reg = context['mapped_io'][addr]
+            op = next(op for op in insn.operands if op.op_type == OpType.ADDR_IO)
+
+            token = next(tokens.index(t) for t in tokens if t.value == op.value)
+            tokens[token] = InstructionTextToken(
+                InstructionTextTokenType.DataSymbolToken,
+                io_reg['name'],
+                io_reg['addr'],
+            )
+
+        return (tokens, length)
+
+    def get_instruction_text(
+        self, data: bytes, addr: int, insn: Instruction | None = None
+    ) -> tuple[list[InstructionTextToken], int] | None:
+        if insn is None:
+            try:
+                insn = Instruction.decode(data)
+            except ValueError:
+                return None
 
         tokens = [
             InstructionTextToken(InstructionTextTokenType.InstructionToken, insn.mnem),
@@ -157,7 +315,7 @@ class AVRArch(Architecture):
                     if len(op.index) in (7, 12):
                         tokens.append(
                             InstructionTextToken(
-                                InstructionTextTokenType.CodeRelativeAddressToken,
+                                InstructionTextTokenType.PossibleAddressToken,
                                 hex(addr + op.value),
                                 addr + op.value,
                             )
@@ -173,11 +331,11 @@ class AVRArch(Architecture):
                         )
 
                 case OpType.ADDR_IO:
-                    # TODO: Display correct IO register names
                     tokens.append(
                         InstructionTextToken(
                             InstructionTextTokenType.PossibleAddressToken,
                             hex(op.value),
+                            op.value,
                         )
                     )
 
