489901a3ff0a6deaecf27019f920f05dc48784f3
m1stadev <adamhamdi31@gmail.com>
2026-07-13 18:29:13 -0500

    plugin: import Operand from base lib

--- binavry/plugin/__init__.py
+++ binavry/plugin/__init__.py
@@ -1,3 +1,3 @@
-from .. import Instruction, Instructions, OpType, get_base_insn
+from .. import Instruction, Instructions, Operand, OpType, get_base_insn
 from .arch import AVRArch
 from .view import AVRView
