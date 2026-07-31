2ff059142afffe7312755431c21632e99e1713f7
m1stadev <adamhamdi31@gmail.com>
2026-07-31 18:30:47 -0500

    compat: add get_instruction_data(), fix incorrect ver

--- binavry/plugin/compat.py
+++ binavry/plugin/compat.py
@@ -1,12 +1,28 @@
-from binaryninja import BasicBlock, BasicBlockAnalysisContext, core_version_info
+from binaryninja import (
+    BasicBlock,
+    BasicBlockAnalysisContext,
+    FunctionLifterContext,
+    core_version_info,
+)
 
 _version = core_version_info().build
 
 
 def add_instruction_data(
-    ctx: BasicBlockAnalysisContext, block: BasicBlock, data: bytes
+    ctx: BasicBlockAnalysisContext | FunctionLifterContext,
+    block: BasicBlock,
+    data: bytes,
 ) -> None:
-    if _version >= 10170:
+    if _version >= 10171:
         ctx.lifter_instruction_data.append(block=block, data=data)
     else:
         block.add_instruction_data(data=data)
+
+
+def get_instruction_data(
+    ctx: BasicBlockAnalysisContext | FunctionLifterContext, block: BasicBlock, addr: int
+) -> bytes:
+    if _version >= 10171:
+        return ctx.lifter_instruction_data.get(block=block, addr=addr)
+    else:
+        return block.get_instruction_data(addr)
